de98f5c9aaa4de759311bad4338b0c1de8499df7
walshie4 <adam@antumbra.io>
2015-02-05 12:29:54 -0500

    Added devicemanager class

---
+++ Antumbra/Connector/DeviceManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antumbra.Glow.Connector
+{
+    /// <summary>
+    /// Manages dealing with connected Glow units via the SerialConnector class.
+    /// Also manages device status.
+    /// </summary>
+    class DeviceManager
+    {
+    }
+}
