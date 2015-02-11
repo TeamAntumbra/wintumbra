3d112f74b9667a662a50109bfd9a9f6c346cde44
walshie4 <adam@antumbra.io>
2015-02-11 16:59:56 -0500

    Added new extension Brightener, raises lightness of output colors by up to 15% or to full if less than 15% away. Will be configurable soon.

---
+++ Brightener/Brightener/Brightener.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Antumbra.Glow.ExtensionFramework;
+using Antumbra.Glow.Utility;
+using System.Threading;
+using System.Drawing;
+
+namespace Brightener
+{
+    [Export(typeof(GlowExtension))]
+    public class Brightener : GlowDecorator
+    {
+        public override int id { get; set; }
+        private bool running;
+        public override string Name
+        {
+            get { return "Brightener"; }
+        }
+
+        public override string Author
+        {
+            get { return "Team Antumbra"; }
+        }
+
+        public override string Description
+        {
+            get { return "Brightens colors using the HSL color model."; }
+        }
+
+        public override bool IsDefault
+        {
+            get { return false; }
+        }
+
+        public override Color Decorate(Color origColor)
+        {
+            HslColor hsl = new HslColor(origColor);
+            if (hsl.L > (1.0 - .15))//TODO make the .15 (or amount lightness is upped) be configurable
+                hsl.L = 1.0;
+            else
+                hsl.L += .15;
+            return hsl.ToRgbColor();
+        }
+
+        public override bool IsRunning
+        {
+            get { return this.running; }
+        }
+
+        public override bool Start()
+        {
+            this.running = false;
+            return true;
+        }
+
+        public override bool Stop()
+        {
+            this.running = true;
+            return true;
+        }
+
+        public override Version Version
+        {
+            get { return new Version("0.0.1"); }
+        }
+
+        public override string Website
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override Dictionary<string, object> Settings
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
