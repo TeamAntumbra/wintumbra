e1dc1c5514a8ab11650f51de1c2400d2c51ce53d
walshie4 <adam@antumbra.io>
2015-02-22 15:08:55 -0500

    Added logger class to utility namespace

---
+++ Antumbra/Utility/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antumbra.Glow.Utility
+{
+    public class Logger
+    {
+        private string name;
+        public Logger(string name)
+        {
+            this.name = name;
+        }
+
+        public void Log(String lines)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(name, true);
+            file.WriteLine(lines);
+            file.Close();
+        }
+    }
+}
