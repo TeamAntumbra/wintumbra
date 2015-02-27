b872d7635d015858884b7147af5b2da9b1c27d59
walshie4 <adam@antumbra.io>
2015-02-27 11:04:28 -0500

    Resolved #7

--- Antumbra/Program.cs
+++ Antumbra/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Antumbra.Glow
 {
@@ -14,16 +15,24 @@ namespace Antumbra.Glow
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            AntumbraCore core = new AntumbraCore();
-            core.Hide();
-            if (core.goodStart)
-                Application.Run();
-            else {//if start up failed close the app before it even starts
-                core.Close();
-                core.Dispose();
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid)) {
+                if (!mutex.WaitOne(0, false)) {
+                    MessageBox.Show("Instance already running");
+                    return;
+                }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                AntumbraCore core = new AntumbraCore();
+                core.Hide();
+                if (core.goodStart)
+                    Application.Run();
+                else {//if start up failed close the app before it even starts
+                    core.Close();
+                    core.Dispose();
+                }
             }
         }
+
+        private static string appGuid = "5e20e0ce-ca88-4e48-b4da-a5de166f5a3d";
     }
 }
