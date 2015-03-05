ead5cd48c03faeb404b9e3ba87cf283f917a0d6e
walshie4 <adam@antumbra.io>
2015-03-05 17:05:50 -0500

    Added interfaces for observers of glow extension collections

---
+++ Antumbra/Observer/Extensions/GlowExtCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antumbra.Glow.Observer.Extensions
+{
+    public interface GlowExtCollection
+    {
+        void AttachGlowExtCollectionObserver(GlowExtCollectionObserver observer);
+    }
+}
---
+++ Antumbra/Observer/Extensions/GlowExtCollectionObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antumbra.Glow.ExtensionFramework;
+
+namespace Antumbra.Glow.Observer.Extensions
+{
+    public interface GlowExtCollectionObserver
+    {
+        void LibraryUpdate(List<GlowExtension> extensions);
+    }
+}
