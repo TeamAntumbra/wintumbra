e51c78a7ed7376905441d4bb64932113a828fb4f
walshie4 <adam@antumbra.io>
2015-03-12 17:38:17 -0400

    Init commit of extensionNotFoundException

---
+++ Antumbra/Exceptions/ExtensionNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antumbra.Glow.Exceptions
+{
+    class ExtensionNotFoundException : Exception
+    {
+        public ExtensionNotFoundException(String extensionName) :
+            base("The extension " + extensionName + " was not found.")
+        {
+            //do nothing, use parent constructor
+        }
+    }
+}
