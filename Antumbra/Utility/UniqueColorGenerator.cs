a430b35f4d7d69a5a9138193eee2188d0d1ee551
walshie4 <adam@antumbra.io>
2015-03-15 23:19:05 -0400

    Added unique color generator utility

---
+++ Antumbra/Utility/UniqueColorGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Antumbra.Glow.Utility
+{
+    public class UniqueColorGenerator
+    {
+        private static UniqueColorGenerator instance;
+        public static UniqueColorGenerator GetInstance()
+        {
+            if (instance == null)
+                instance = new UniqueColorGenerator();
+            return instance;
+        }
+        private List<HslColor> assigned;
+        public UniqueColorGenerator()
+        {
+            this.assigned = new List<HslColor>();
+        }
+
+        public Color GetUniqueColor()
+        {
+            Color result = Color.Empty;
+            List<double> hues = new List<double>();
+            foreach (HslColor col in this.assigned)
+                hues.Add(col.H);
+            int count = hues.Count;
+            if (count == 0) {
+                result = new HslColor(0, 1.0, .5);
+            }
+            else {
+                double prev = 0;
+                double gapStart = 0;
+                double largestGap = 180.0;
+                for (int i = 1; i < count; i += 1) {
+                    double current = hues[i];
+                    double gap = Math.Abs(prev-current);
+                    if (gap > largestGap) {
+                        largestGap = gap;
+                        gapStart = current;
+                    }
+                }
+                result = new HslColor(gapStart + (largestGap / 2.0), 1.0, .5);
+            }
+            this.assigned.Add(result);
+            return result;
+        }
+    }
+}
