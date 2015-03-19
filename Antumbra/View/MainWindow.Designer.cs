1ac96a20ea774215222d68e9cc9914d836f24241
walshie4 <adam@antumbra.io>
2015-03-18 20:09:48 -0400

    Init commit of new UI/UX rework main window

---
+++ Antumbra/View/MainWindow.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Antumbra.Glow.View
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.flatTabControl = new FlatTabControl.FlatTabControl();
+            this.manualTab = new System.Windows.Forms.TabPage();
+            this.mirrorTab = new System.Windows.Forms.TabPage();
+            this.flatTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.AutoSize = true;
+            this.closeBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.closeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.closeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.closeBtn.Location = new System.Drawing.Point(720, -2);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(28, 28);
+            this.closeBtn.TabIndex = 73;
+            this.closeBtn.Text = "X";
+            this.closeBtn.UseVisualStyleBackColor = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // flatTabControl
+            // 
+            this.flatTabControl.Controls.Add(this.manualTab);
+            this.flatTabControl.Controls.Add(this.mirrorTab);
+            this.flatTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.flatTabControl.Location = new System.Drawing.Point(14, 48);
+            this.flatTabControl.myBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.flatTabControl.Name = "flatTabControl";
+            this.flatTabControl.SelectedIndex = 0;
+            this.flatTabControl.Size = new System.Drawing.Size(755, 348);
+            this.flatTabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.flatTabControl.TabIndex = 74;
+            // 
+            // manualTab
+            // 
+            this.manualTab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.manualTab.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.manualTab.Location = new System.Drawing.Point(4, 25);
+            this.manualTab.Name = "manualTab";
+            this.manualTab.Padding = new System.Windows.Forms.Padding(3);
+            this.manualTab.Size = new System.Drawing.Size(747, 319);
+            this.manualTab.TabIndex = 0;
+            this.manualTab.Text = "Manual";
+            // 
+            // mirrorTab
+            // 
+            this.mirrorTab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.mirrorTab.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.mirrorTab.Location = new System.Drawing.Point(4, 25);
+            this.mirrorTab.Name = "mirrorTab";
+            this.mirrorTab.Padding = new System.Windows.Forms.Padding(3);
+            this.mirrorTab.Size = new System.Drawing.Size(747, 319);
+            this.mirrorTab.TabIndex = 1;
+            this.mirrorTab.Text = "Mirror";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(783, 411);
+            this.Controls.Add(this.flatTabControl);
+            this.Controls.Add(this.closeBtn);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainWindow";
+            this.flatTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeBtn;
+        private FlatTabControl.FlatTabControl flatTabControl;
+        private System.Windows.Forms.TabPage manualTab;
+        private System.Windows.Forms.TabPage mirrorTab;
+    }
+}
