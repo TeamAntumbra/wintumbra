a8dac753a703e0d79a7d4b8cc3d99fb8d77914c3
walshie4 <adam@antumbra.io>
2015-03-18 20:12:05 -0400

    Init commit of new UI/UX rework main window

---
+++ Antumbra/View/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Antumbra.Glow.View
+{
+    public partial class MainWindow : Form
+    {
+        public event EventHandler closeBtn_ClickEvent;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
