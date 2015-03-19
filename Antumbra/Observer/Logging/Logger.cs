1cb474e32ed9b484ba39044ee7098cdcb7214593
walshie4 <adam@antumbra.io>
2015-03-18 21:41:08 -0400

    logger is now an observer singleton, will observe those who need to write messages to the log

--- Antumbra/Observer/Logging/Logger.cs
+++ Antumbra/Observer/Logging/Logger.cs
@@ -9,46 +9,53 @@ namespace Antumbra.Glow.Observer.Logging
     /// <summary>
     /// Allows easy logging to a specified file
     /// </summary>
-    public class Logger
+    public static class LoggerHelper
     {
-        private object sync = new object();
-        /// <summary>
-        /// Name of the log file for this Logger
-        /// </summary>
-        private string name;
-        /// <summary>
-        /// Constructor - Create a new logger with the logfile at path as passed
-        /// </summary>
-        /// <param name="name"></param>
-        private string path;
-        private static Logger instance;
+        internal class Logger : LogMsgObserver {
+            private object sync = new object();
+            /// <summary>
+            /// Name of the log file for this Logger
+            /// </summary>
+            private string name;
+            private string path;
+            public Logger(string name) {
+                this.name = name;
+                this.path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                    "\\Antumbra\\";
+                if (!System.IO.Directory.Exists(this.path))
+                    System.IO.Directory.CreateDirectory(this.path);
+            }
 
-        public static Logger GetInstance()
+            public void NewLogMsgAvail(String source, String msg)
+            {
+                this.Log(source + " - " + msg);
+            }
+
+            /// <summary>
+            /// Write the passed lines to the log file
+            /// </summary>
+            /// <param name="lines"></param>
+            private void Log(String lines)
+            {
+                lock (sync) {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(this.path + name, false)) {
+                        file.WriteLine(lines);
+                    }
+                }
+            }
+        }
+
+        private static Logger instance;
+        private static void AttachLoggable(Loggable loggable)
+        {
+
+        }
+
+        private static Logger GetInstance()
         {
             if (instance == null)
                 instance = new Logger("wintumbra.log");
             return instance;
-        }
-
-        private Logger(string name)
-        {
-            this.name = name;
-            this.path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
-                "\\Antumbra\\";
-            if (!System.IO.Directory.Exists(this.path))
-                System.IO.Directory.CreateDirectory(this.path);
-        }
-        /// <summary>
-        /// Write the passed lines to the log file
-        /// </summary>
-        /// <param name="lines"></param>
-        public void Log(String lines)
-        {
-            lock (sync) {
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(this.path + name, false)) {
-                    file.WriteLine(lines);
-                }
-            }
         }
     }
 }
