bfcf6d92c2832b758c145b78e0e8478bed616433
walshie4 <adam@antumbra.io>
2015-03-22 19:17:59 -0400

    Init commit of pollingAreaWindowManager

---
+++ Antumbra/Settings/PollingAreaWindowManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antumbra.Glow.Controller;
+using Antumbra.Glow.Observer.GlowCommands;
+
+namespace Antumbra.Glow.Settings
+{
+    public class PollingAreaWindowManager : GlowCommandSender, GlowCommandObserver
+    {
+        public delegate void NewGlowCmdAvail(GlowCommand cmd);
+        public event NewGlowCmdAvail NewGlowCommandAvailEvent;
+        private List<PollingAreaWindowController> controllers;
+        public PollingAreaWindowManager()
+        {
+            this.controllers = new List<PollingAreaWindowController>();
+        }
+
+        public void CreateAndAddController(int id)
+        {
+            PollingAreaWindowController cont = new PollingAreaWindowController();
+            cont.AttachObserver(this);//attach to pass through
+            this.controllers.Add(cont);
+        }
+
+        public void RegisterDevice(int id)
+        {
+            //ignore, simply a pass through not registered to a single device
+        }
+
+        public void NewGlowCommandAvail(GlowCommand cmd)
+        {
+            if (NewGlowCommandAvailEvent != null)
+                NewGlowCommandAvailEvent(cmd);//pass up
+        }
+
+        public void AttachObserver(GlowCommandObserver observer)
+        {
+            this.NewGlowCommandAvailEvent += observer.NewGlowCommandAvail;
+        }
+    }
+}
