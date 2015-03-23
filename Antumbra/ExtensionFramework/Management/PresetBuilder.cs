04cf998699d870ec6ce1d0fa51e7a9f314584c13
walshie4 <adam@antumbra.io>
2015-03-23 18:34:52 -0400

    Added placeholder for neonFade driver's GUID in presetBuilder, updated main csproj

--- Antumbra/ExtensionFramework/Management/PresetBuilder.cs
+++ Antumbra/ExtensionFramework/Management/PresetBuilder.cs
@@ -11,16 +11,16 @@ namespace Antumbra.Glow.ExtensionFramework.Management
     public class PresetBuilder
     {
         private ExtensionLibrary lib;
-        private const Guid HSVDriver = Guid.Parse("8360550b-d599-4f0f-8806-bc323f9ce547");
-        private const Guid SinDriver = Guid.Parse("31cae25b-72c0-4ffc-860b-234fb931bc15");
-        private const Guid NeonDriver = Guid.Parse("");//TODO
-        private const Guid ScreenDriverCoupler = Guid.Parse("70987576-1a00-4a34-b787-4c08516cd1b8");
-        private const Guid ScreenGrabber = Guid.Parse("15115e91-ed5c-49e6-b7a8-4ebbd4dabb2e");
-        private const Guid SmartProcessor = Guid.Parse("f4f3692d-a405-4dca-82fd-a9f3e9f93afe");
-        private const Guid FastProcessor = Guid.Parse("07eda8bc-28e6-4d57-a085-7f204785630f");
-        private const Guid DXGrabber = Guid.Parse("ae53796b-ac50-4cef-a335-2d75dea9f1ea");
-        private const Guid Saturator = Guid.Parse("2acba4a6-af21-47a9-9551-964a750fea06");
-        private const Guid Brightener = Guid.Parse("1a271e63-5f7e-43c0-bbb1-7d80d23d8db7");
+        private Guid HSVDriver = Guid.Parse("8360550b-d599-4f0f-8806-bc323f9ce547");
+        private Guid SinDriver = Guid.Parse("31cae25b-72c0-4ffc-860b-234fb931bc15");
+        private Guid NeonDriver = Guid.Empty;//Guid.Parse("");//TODO
+        private Guid ScreenDriverCoupler = Guid.Parse("70987576-1a00-4a34-b787-4c08516cd1b8");
+        private Guid ScreenGrabber = Guid.Parse("15115e91-ed5c-49e6-b7a8-4ebbd4dabb2e");
+        private Guid SmartProcessor = Guid.Parse("f4f3692d-a405-4dca-82fd-a9f3e9f93afe");
+        private Guid FastProcessor = Guid.Parse("07eda8bc-28e6-4d57-a085-7f204785630f");
+        private Guid DXGrabber = Guid.Parse("ae53796b-ac50-4cef-a335-2d75dea9f1ea");
+        private Guid Saturator = Guid.Parse("2acba4a6-af21-47a9-9551-964a750fea06");
+        private Guid Brightener = Guid.Parse("1a271e63-5f7e-43c0-bbb1-7d80d23d8db7");
         public PresetBuilder(ExtensionLibrary lib)
         {
             this.lib = lib;
