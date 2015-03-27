132401d32279bdb06d38a94616cdd2030d9965a8
walshie4 <adam@antumbra.io>
2015-03-27 14:40:54 -0400

    added event handlers and various contstuctors for white balance controller #14

--- Antumbra/Controller/WhiteBalanceWindowController.cs
+++ Antumbra/Controller/WhiteBalanceWindowController.cs
@@ -4,15 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Antumbra.Glow.View;
+using Antumbra.Glow.Connector;
 
 namespace Antumbra.Glow.Controller
 {
     public class WhiteBalanceWindowController
     {
         private WhiteBalanceWindow view;
-        public WhiteBalanceWindowController()
+        private List<GlowDevice> devices;
+        public WhiteBalanceWindowController(GlowDevice dev)
+        {
+            this.devices = new List<GlowDevice>();
+            this.devices.Add(dev);
+            Init();
+        }
+
+        public WhiteBalanceWindowController(List<GlowDevice> devs)
+        {
+            this.devices = new List<GlowDevice>();
+            foreach (var dev in devs)
+                this.devices.Add(dev);
+            Init();
+        }
+
+        private void Init()
         {
             this.view = new WhiteBalanceWindow();
+            this.view.rDownBtn_ClickEvent += new EventHandler(redDownHandler);
+            this.view.gDownBtn_ClickEvent += new EventHandler(greenDownHandler);
+            this.view.bDownBtn_ClickEvent += new EventHandler(blueDownHandler);
+            this.view.rUpBtn_ClickEvent += new EventHandler(redUpHandler);
+            this.view.gUpBtn_ClickEvent += new EventHandler(greenUpHandler);
+            this.view.bUpBtn_clickEvent += new EventHandler(blueUpHandler);
+        }
+
+        private void redDownHandler(object sender, EventArgs args)
+        {
+            foreach (GlowDevice dev in devices)
+                dev.settings.redBias -= 1;
+        }
+
+        private void greenDownHandler(object sender, EventArgs args)
+        {
+            foreach (GlowDevice dev in devices)
+                dev.settings.greenBias -= 1;
+        }
+
+        private void blueDownHandler(object sender, EventArgs args)
+        {
+            foreach (GlowDevice dev in devices)
+                dev.settings.blueBias -= 1;
+        }
+
+        private void redUpHandler(object sender, EventArgs args)
+        {
+            foreach (GlowDevice dev in devices)
+                dev.settings.redBias += 1;
+        }
+
+        private void greenUpHandler(object sender, EventArgs args)
+        {
+            foreach (GlowDevice dev in devices)
+                dev.settings.greenBias += 1;
+        }
+
+        private void blueUpHandler(object sender, EventArgs args)
+        {
+            foreach (GlowDevice dev in devices)
+                dev.settings.blueBias += 1;
         }
 
         public void Show()
