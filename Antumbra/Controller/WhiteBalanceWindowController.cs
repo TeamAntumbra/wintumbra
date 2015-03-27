2c584097196821a308b5e48ef80269f8f1822701
walshie4 <adam@antumbra.io>
2015-03-27 16:15:06 -0400

    Added configurationObserver interface to white balance controller and added code to attach itself to the devices it is balancing.

--- Antumbra/Controller/WhiteBalanceWindowController.cs
+++ Antumbra/Controller/WhiteBalanceWindowController.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Antumbra.Glow.View;
 using Antumbra.Glow.Connector;
+using Antumbra.Glow.Observer.Configuration;
 
 namespace Antumbra.Glow.Controller
 {
-    public class WhiteBalanceWindowController
+    public class WhiteBalanceWindowController : ConfigurationObserver
     {
         private WhiteBalanceWindow view;
         private List<GlowDevice> devices;
@@ -16,18 +17,35 @@ namespace Antumbra.Glow.Controller
         {
             this.devices = new List<GlowDevice>();
             this.devices.Add(dev);
+            dev.AttachObserver(this);
         }
 
         public WhiteBalanceWindowController(List<GlowDevice> devs)
         {
             this.devices = new List<GlowDevice>();
-            foreach (var dev in devs)
+            foreach (var dev in devs) {
                 this.devices.Add(dev);
+                dev.AttachObserver(this);
+            }
+        }
+
+        public void ConfigurationUpdate(Configurable config)
+        {
+            if (config is Settings.DeviceSettings) {
+                Settings.DeviceSettings settings = (Settings.DeviceSettings)config;
+                this.view.SetR(settings.redBias);
+                this.view.SetG(settings.greenBias);
+                this.view.SetB(settings.blueBias);
+            }
         }
 
         private void Init()
         {
             this.view = new WhiteBalanceWindow();
+            GlowDevice dev = this.devices.First<GlowDevice>();
+            this.view.SetR(dev.settings.redBias);
+            this.view.SetG(dev.settings.greenBias);
+            this.view.SetB(dev.settings.blueBias);
             this.view.rDownBtn_ClickEvent += new EventHandler(redDownHandler);
             this.view.gDownBtn_ClickEvent += new EventHandler(greenDownHandler);
             this.view.bDownBtn_ClickEvent += new EventHandler(blueDownHandler);
