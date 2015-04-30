6c89091912c988595523ff7c75471baad8bd584d
walshie4 <adam@antumbra.io>
2015-04-30 17:14:58 -0400

    Added FastBitmap class to utility classes for easier manipulation of Bitmaps at speed, as well as easier integration of Bitmap decorators who can work directly on the bits at speed.

---
+++ Antumbra/Utility/FastBitmap.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Antumbra.Glow.Utility
+{
+    public class FastBitmap
+    {
+        private Bitmap bitmap;
+        private BitmapData data;
+        private bool locked;
+        private byte[] pxData;
+        private IntPtr scan0;
+        private int height, width, depth;
+        public FastBitmap(Bitmap bm)
+        {
+            this.bitmap = bm;
+        }
+        /*
+         * Locks the underlying Bitmap object for faster manipulation of bits.
+         */
+        public void Lock()
+        {
+            try {
+                Size size = this.bitmap.Size;
+                this.width = size.Width;
+                this.height = size.Height;
+                PixelFormat pxFormat = this.bitmap.PixelFormat;
+                this.depth = Bitmap.GetPixelFormatSize(pxFormat);
+                if (this.depth != 8 && this.depth != 24 && this.depth != 32) {
+                    throw new ArgumentException("Only 8, 24 and 32 bpp images are supported.");
+                }
+                this.data = this.bitmap.LockBits(new Rectangle(new Point(0, 0), size), ImageLockMode.ReadOnly,
+                    pxFormat);
+                int step = this.depth / 8;
+                this.pxData = new byte[step * this.width * this.height];
+                this.scan0 = this.data.Scan0;
+                Marshal.Copy(this.scan0, this.pxData, 0, this.pxData.Length);
+                this.locked = true;
+            }
+            catch(Exception e) {
+                //TODO
+                throw e;
+            }
+        }
+        /*
+         * Unlocks the BitmapData for the underlying Bitmap object.
+         */
+        public void Unlock()
+        {
+            try {
+                Marshal.Copy(this.pxData, 0, this.scan0, this.pxData.Length);
+                this.bitmap.UnlockBits(this.data);
+            }
+            catch (Exception e) {
+                //TODO
+                throw e;
+            }
+        }
+        /*
+         * Returns color info in a byte array of form [(ALPHA,) RED, GREEN, BLUE]
+         * for the pixel found @ location (x,y).
+         */
+        public byte[] GetPixel(int x, int y) {
+            byte[] color;
+            int compCount = this.depth / 8;
+            int startLoc = (y * this.width + x) * compCount;
+            switch (this.depth) {
+                case 32:
+                    color = new byte[4];
+                    color[0] = this.pxData[startLoc + 3];//Alpha
+                    color[1] = this.pxData[startLoc + 2];//Red
+                    color[2] = this.pxData[startLoc + 1];//Green
+                    color[3] = this.pxData[startLoc];//Blue
+                    return color;
+                case 16:
+                    color = new byte[3];
+                    color[0] = this.pxData[startLoc + 2];//Red
+                    color[1] = this.pxData[startLoc + 1];//Green
+                    color[2] = this.pxData[startLoc];//Blue
+                    return color;
+                case 8:
+                    color = new byte[3];
+                    byte value = this.pxData[startLoc];//all the same on 8 bit
+                    color[0] = value;
+                    color[1] = value;
+                    color[2] = value;
+                    return color;
+                default:
+                    throw new Exception("invalid bitmap depth found in FastBitmap");
+            }
+        }
+    }
+}
