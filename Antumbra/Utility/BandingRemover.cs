506d5f2a1a77b21e70c6620e3315fd12563203e9
walshie4 <adam@antumbra.io>
2015-05-01 12:53:45 -0400

    Moved each banding check to it's own method, added locked check to stop from trying to lock again (InvalidOperationException), readied for move to FastBitmap throughout system.

--- Antumbra/Utility/BandingRemover.cs
+++ Antumbra/Utility/BandingRemover.cs
@@ -18,57 +18,35 @@ namespace Antumbra.Glow.Utility
     public static class BandingRemover
     {
         /*
-         * Same as previous method, removes all banding from passed image,
+         * Removes all banding from passed image,
          * however this function takes in the byte array of pixel data
          * found using the Bitmap.LockBits() method of Bitmap manipulation.
          */
         public static FastBitmap RemoveBanding(FastBitmap orig)
         {
-            int top = -1, bottom = -1, left = -1, right = -1;//amount of rows/cols to remove in each dir
-            bool topBuilding = false;//done building the top bar yet?
-            for (int row = 0; row < orig.height; row += 1) {//for each row
-                bool blackRow = true;
-                int leftBlack = 0;
-                bool leftBuilding = true;
-                int rightBlack = 0;
-                for (int col = 0; col < orig.width; col += 1) {//for each column
-                    byte[] pixel = orig.GetPixel(row, col);//pixel info
-                    int length = pixel.Length;
-                    if (pixel[length - 3] == 0 && pixel[length - 2] == 0 && pixel[length -1] == 0) {//black pixel
-                        if (leftBuilding)//left side black border still being built
-                            left += 1;
-                        rightBlack += 1;
-                    }
-                    else {
-                        topBuilding = false;
-                        blackRow = false;
-                        leftBuilding = false;
-                        rightBlack = 0;
-                    }
-                }
-                if (blackRow) {
-                    if (topBuilding)
-                        top += 1;
-                    bottom += 1;
-                }
-                else
-                    bottom = 0;
-                if (left == -1)//has yet to be set
-                    left = leftBlack;
-                else
-                    left = Math.Min(left, leftBlack);
+            int top, bottom, left, right;
+            try {
+                top = FindTopBanding(orig);
+                bottom = FindBottomBanding(orig);
+                left = FindLeftBanding(orig);
+                right = FindRightBanding(orig);
             }
-            if (top <= 0 && bottom <= 0 && left <= 0 && right <= 0)//no change
+            catch (Exception e) {
+                throw e;
+            }
+            if (top == 0 && bottom == 0 && left == 0 && right == 0)//no cropping needed
                 return orig;
+            Console.WriteLine(top + " " + bottom + " " + left + " " + right);
             //else crop and return cropped
             int newWidth = orig.width - left - right;
             int newHeight = orig.height - top - bottom;
-            byte[] newBytes = new byte[newWidth * newHeight * orig.BPP];
+            int BPP = orig.depth / 8;
+            byte[] newBytes = new byte[newWidth * newHeight * BPP];
             for (int row = 0; row < orig.height; row += 1) {
-                for (int col = 0; col < orig.width * orig.BPP; col += orig.BPP) {
-                    int origIndex = (left * orig.data.Stride) + (row * orig.data.Stride) + (top * orig.BPP) + col;
-                    int newIndex = row * newWidth * orig.BPP + col;
-                    for (int bit = 0; bit < orig.BPP; bit += 1)
+                for (int col = 0; col < orig.width * BPP; col += BPP) {
+                    int origIndex = (left * orig.data.Stride) + (row * orig.data.Stride) + (top * BPP) + col;
+                    int newIndex = row * newWidth * BPP + col;
+                    for (int bit = 0; bit < BPP; bit += 1)
                         newBytes[newIndex + bit] = orig.pxData[origIndex + bit];
                 }
             }
@@ -78,6 +56,92 @@ namespace Antumbra.Glow.Utility
             Marshal.Copy(newBytes, 0, resultData.Scan0, newBytes.Length);
             orig.Dispose();
             return new FastBitmap(resultBm);
+        }
+
+        public static bool IsBlackPixel(byte[] pixelInfo)
+        {
+            int len = pixelInfo.Length;
+            if (len == 4 || len == 3) {
+                return (pixelInfo[len - 3] == 0 && pixelInfo[len - 2] == 0 && pixelInfo[len - 1] == 0);
+            }
+            else
+                throw new ArgumentException("pixelInfo array not of size 3 or 4 as expected.");
+        }
+
+        public static int FindTopBanding(FastBitmap bm)
+        {
+            int blackRows = 0;
+            for (int row = 0; row < bm.height - 1; row += 1) {
+                bool blackRow = true;
+                for (int col = 0; col < bm.width - 1; col += 1) {
+                    byte[] pixel = bm.GetPixel(col, row);
+                    int length = pixel.Length;
+                    if (!IsBlackPixel(pixel))//not a black pixel
+                        blackRow = false;
+                }
+                if (blackRow)
+                    blackRows += 1;
+                else
+                    return blackRows;
+            }
+            return blackRows;
+        }
+
+        public static int FindLeftBanding(FastBitmap bm)
+        {
+            int blackCols = 0;
+            for (int col = 0; col < bm.width - 1; col += 1) {
+                bool blackCol = true;
+                for (int row = 0; row < bm.height - 1; row += 1) {
+                    byte[] pixel = bm.GetPixel(col, row);
+                    int len = pixel.Length;
+                    if (!IsBlackPixel(pixel))
+                        blackCol = false;
+                }
+                if (blackCol)
+                    blackCols += 1;
+                else
+                    return blackCols;
+            }
+            return blackCols;
+        }
+
+        public static int FindRightBanding(FastBitmap bm)
+        {
+            int blackCols = 0;
+            for (int col = bm.width - 1; col > 0; col -= 1) {//right to left
+                bool blackCol = true;
+                for (int row = 0; row < bm.height - 1; row += 1) {
+                    byte[] pixel = bm.GetPixel(col, row);
+                    int len = pixel.Length;
+                    if (!IsBlackPixel(pixel))
+                        blackCol = false;
+                }
+                if (blackCol)
+                    blackCols += 1;
+                else
+                    return blackCols;
+            }
+            return blackCols;
+        }
+
+        public static int FindBottomBanding(FastBitmap bm)
+        {
+            int blackRows = 0;
+            for (int row = bm.height - 1; row > 0; row -= 1) {//bottom to top
+                bool blackRow = true;
+                for (int col = 0; col < bm.width - 1; col += 1) {
+                    byte[] pixel = bm.GetPixel(col, row);
+                    int length = pixel.Length;
+                    if (!IsBlackPixel(pixel))//not a black pixel
+                        blackRow = false;
+                }
+                if (blackRow)
+                    blackRows += 1;
+                else
+                    return blackRows;
+            }
+            return blackRows;
         }
     }
 }
