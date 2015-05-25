a44b2e1774f630dc2fb2dd6a6828e225f214e1e0
walshie4 <adam@antumbra.io>
2015-05-25 14:20:40 -0400

    Added p/invoke use of MoveWindow to set location and bounds of polling window because Microsoft chose to leave the SetBounds and similar Form methods broken... :|

--- Antumbra/Controller/PollingAreaWindowController.cs
+++ Antumbra/Controller/PollingAreaWindowController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using Antumbra.Glow.Utility;
 using Antumbra.Glow.Observer.GlowCommands;
 using Antumbra.Glow.Observer.GlowCommands.Commands;
@@ -33,7 +34,12 @@ namespace Antumbra.Glow.Controller
             SendCommand(new StopCommand(this.id));//stop device
             SendCommand(new SendColorCommand(this.id,new Color16Bit(this.color)));//set to unique color to match its window
         }
-        
+
+        public void SetBounds(int x, int y, int width, int height)
+        {
+            MoveWindow(this.pollingWindow.Handle, x, y, width, height, true);
+        }
+
         private void pollingArea_Click(object sender, EventArgs e)
         {
             if (this.pollingWindow == null || this.pollingWindow.IsDisposed) {
@@ -67,10 +73,24 @@ namespace Antumbra.Glow.Controller
             if (sender is System.Windows.Forms.Form) {
                 System.Windows.Forms.Form form = (System.Windows.Forms.Form)sender;
                 if (PollingAreaUpdatedEvent != null)
-                    PollingAreaUpdatedEvent(form.Bounds.X, form.Bounds.Y, form.Width, form.Height);
+                    PollingAreaUpdatedEvent(form.Location.X, form.Location.Y, form.Width, form.Height);
                 UniqueColorGenerator.GetInstance().RetireUniqueColor(form.BackColor);
-                SendCommand(new StartCommand(this.id));
             }
         }
+
+        /// <summary>
+        /// The MoveWindow function changes the position and dimensions of the specified window. For a top-level window, the position and dimensions are relative to the upper-left corner of the screen. For a child window, they are relative to the upper-left corner of the parent window's client area.
+        /// </summary>
+        /// <param name="hWnd">Handle to the window.</param>
+        /// <param name="X">Specifies the new position of the left side of the window.</param>
+        /// <param name="Y">Specifies the new position of the top of the window.</param>
+        /// <param name="nWidth">Specifies the new width of the window.</param>
+        /// <param name="nHeight">Specifies the new height of the window.</param>
+        /// <param name="bRepaint">Specifies whether the window is to be repainted. If this parameter is TRUE, the window receives a message. If the parameter is FALSE, no repainting of any kind occurs. This applies to the client area, the nonclient area (including the title bar and scroll bars), and any part of the parent window uncovered as a result of moving a child window.</param>
+        /// <returns>If the function succeeds, the return value is nonzero.
+        /// <para>If the function fails, the return value is zero. To get extended error information, call GetLastError.</para></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
     }
 }
