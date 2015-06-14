4dd2e64c69ea766ccebf56427857700a7b2053ce
walshie4 <adam@antumbra.io>
2015-06-14 00:34:09 -0400

    NewScreenAvailEvent is now fired with a Bitmap to potentially allow cropping for unified capturer implementation

--- Antumbra/Observer/Bitmaps/AntumbraBitmapObserver.cs
+++ Antumbra/Observer/Bitmaps/AntumbraBitmapObserver.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using Antumbra.Glow.Utility;
 
 namespace Antumbra.Glow.Observer.Bitmaps
 {
     public interface AntumbraBitmapObserver
     {
-        void NewBitmapAvail(FastBitmap image, EventArgs args);
+        void NewBitmapAvail(Bitmap image, EventArgs args);
     }
 }
