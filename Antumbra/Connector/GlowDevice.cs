a3e85877192bdbe0abaf5a8f4ab1344c86718b10
walshie4 <adam@antumbra.io>
2015-08-01 21:13:59 -0400

    Removes ExtensionManager related GlowDevice functions and variables

--- Antumbra/Connector/GlowDevice.cs
+++ Antumbra/Connector/GlowDevice.cs
@@ -11,19 +11,15 @@ using Antumbra.Glow.Observer.ToolbarNotifications;
 using Antumbra.Glow.Observer.GlowCommands;
 using Antumbra.Glow.Observer.Colors;
 using Antumbra.Glow.Observer.Configuration;
-using Antumbra.Glow.Utility.Settings;
+using Antumbra.Glow.Utility.Saving;
 
 namespace Antumbra.Glow.Connector
 {
     /// <summary>
     /// Represents a physical Antumbra|Glow unit.
     /// </summary>
-    public class GlowDevice : ConfigurationObserver, Configurable, ToolbarNotificationSource, ToolbarNotificationObserver
+    public class GlowDevice
     {
-        public delegate void ConfigUpdate(Configurable obj);
-        public event ConfigUpdate ConfigUpdateAvail;
-        public delegate void NewToolbarNotif(int time, string title, string msg, int icon);
-        public event NewToolbarNotif NewToolbarNotifEvent;
         /// <summary>
         /// Device pointer
         /// </summary>
@@ -51,50 +47,11 @@ namespace Antumbra.Glow.Connector
         /// <summary>
         /// DeviceSettings obj for this device
         /// </summary>
-        public DeviceSettings settings { get; private set; } //TODO make private
+        public DeviceSettings settings { get; private set; }
         /// <summary>
-        /// ExtensionManager for this device
-        /// </summary>
-        private ExtensionManager extMgr;
-        /// <summary>
-        /// Integer representation of the device's status
+        /// The device's latest status response
         /// </summary>
         public int status { get; set; }
-        //public bool running { get; private set; }
-
-        public void SetActives(ActiveExtensions actives)
-        {
-            actives.AttachObserver(this);
-            this.extMgr.LoadActives(actives);
-        }
-
-        public void SetDvrGbbrOrPrcsrExt(Guid id)
-        {
-            this.extMgr.UpdateExtension(id);
-        }
-
-        public bool SetFiltOrNotf(Guid id)
-        {
-            bool result = this.extMgr.ToggleFiltOrNotf(id);
-            return result;
-        }
-
-        public void Notify()
-        {
-            this.settings.Notify();
-            this.extMgr.activeExts.Notify();
-        }
-
-        public bool GetFiltOrNotfStatus(Guid id)
-        {
-            foreach (GlowFilter filt in this.extMgr.activeExts.ActiveFilters)
-                if (filt.id.Equals(id))
-                    return true;
-            foreach (GlowNotifier notf in this.extMgr.activeExts.ActiveNotifiers)
-                if (notf.id.Equals(id))
-                    return true;
-            return false;
-        }
         /// <summary>
         /// Constructor
         /// </summary>
@@ -107,94 +64,6 @@ namespace Antumbra.Glow.Connector
             this.id = id;
             this.dev = IntPtr.Zero;
             this.settings = new DeviceSettings(id);
-            this.settings.AttachObserver(this);
-            this.extMgr = new ExtensionManager(lib, id);
-            this.extMgr.AttachObserver((ToolbarNotificationObserver)this);
-            this.extMgr.activeExts.AttachObserver(this);
-            this.settings.AttachObserver((ConfigurationObserver)this.extMgr);
-        }
-        /// <summary>
-        /// Start the device's extensions
-        /// </summary>
-        /// <returns>True if successfully started, else false</returns>
-        public bool Start()
-        {
-            this.settings.powerState = this.extMgr.Start();
-            return this.settings.powerState;
-        }
-
-        public void LoadSettings()
-        {
-            this.Stop();
-            Saver saver = Saver.GetInstance();
-            this.settings.LoadSave(saver.Load(this.id.ToString()));
-            ActiveExtensions newActives = (ActiveExtensions)this.extMgr.LoadSave(saver.Load(ExtensionManager.configFileBase + this.id));
-            this.SetActives(newActives);
-        }
-
-        public void Reset()
-        {
-            this.settings.Reset();
-            this.extMgr.Reset();
-            this.extMgr.activeExts.AttachObserver(this);//attach to new actives obj
-            this.Notify();
-        }
-
-        public void SaveSettings()
-        {
-            this.settings.Save();
-            this.extMgr.Save();
-        }
-        /// <summary>
-        /// Attach an AntumbraColorObserver to the underlying extension manager
-        /// </summary>
-        /// <param name="observer"></param>
-        public void AttachColorObserverToExtMgr(AntumbraColorObserver observer)
-        {
-            this.extMgr.AttachObserver(observer);
-        }
-
-        public Color16Bit ApplyFilters(Color16Bit orig)
-        {
-            return this.extMgr.ApplyFilters(orig);
-        }
-
-        public Color16Bit ApplyBrightness(Color16Bit orig)
-        {
-            return this.extMgr.ApplyBrightnessSettings(orig);
-        }
-
-        public Color16Bit ApplyWhiteBalance(Color16Bit orig)
-        {
-            return this.extMgr.ApplyWhiteBalance(orig);
-        }
-
-        public void ApplyDriverRecomSettings()
-        {
-            GlowDriver activeDriver = this.extMgr.activeExts.ActiveDriver;
-            activeDriver.RecmmndCoreSettings();
-            this.settings.stepSleep = activeDriver.stepSleep;
-            this.settings.weightingEnabled = activeDriver.weighted;
-            this.settings.newColorWeight = activeDriver.newColorWeight;
-        }
-
-        public void AttachObserver(ConfigurationObserver o)
-        {
-            this.ConfigUpdateAvail += o.ConfigurationUpdate;
-        }
-
-        public void ConfigurationUpdate(Configurable obj)
-        {
-            if (this.ConfigUpdateAvail != null)
-                this.ConfigUpdateAvail(obj);//pass through
-        }
-        /// <summary>
-        /// Get a string representation of the extensions activated for this device
-        /// </summary>
-        /// <returns>String representation of active extensions.</returns>
-        public string GetSetupDesc()
-        {
-            return extMgr.GetSetupDesc();
         }
         /// <summary>
         /// Return string representation of this device.
@@ -203,21 +72,6 @@ namespace Antumbra.Glow.Connector
         public override string ToString()
         {
             return "Glow device, id: " + this.id;
-        }
-        /// <summary>
-        /// Stop the extensions for this device
-        /// </summary>
-        /// <returns>True if successful, else false</returns>
-        public bool Stop()
-        {
-            bool result = this.extMgr.Stop();
-            this.settings.powerState = !result;//inverse of stop result status is running state
-            return result;
-        }
-
-        public bool GetExtSettingsWin(Guid id)
-        {
-            return this.extMgr.GetExtSettingsWin(id);
         }
 
         public void AttachObserver(ToolbarNotificationObserver observer)
