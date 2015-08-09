6c8ed2d29e1eceb6c55ec9a7d217bae858f4ac26
walshie4 <adam@antumbra.io>
2015-08-09 16:51:22 -0400

    Added summary comments to methods

--- Antumbra/Connector/SerialConnector.cs
+++ Antumbra/Connector/SerialConnector.cs
@@ -18,6 +18,12 @@ namespace Antumbra.Glow.Connector
         {
             byte endpoint;
         }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="vid"></param>
+        /// <param name="pid"></param>
         public SerialConnector(int vid, int pid)
         {
             this.ctx = IntPtr.Zero;
@@ -27,22 +33,56 @@ namespace Antumbra.Glow.Connector
             UpdateDeviceList();
         }
 
+        /// <summary>
+        /// Get the device info pointer for a specified index
+        /// </summary>
+        /// <param name="index">The Glow's index</param>
+        /// <returns></returns>
         public IntPtr GetDeviceInfo(int index)
         {
             return AnDevice_IndexOpaqueList(this.devs, index);
         }
 
+        /// <summary>
+        /// Open a Glow device connection
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="outerr"></param>
+        /// <returns></returns>
         public IntPtr OpenDevice(IntPtr info, out int outerr)
         {
             return AnDevice_OpenReturn(this.ctx, info, out outerr);
         }
 
+        /// <summary>
+        /// Update device list
+        /// </summary>
+        /// <returns></returns>
         public int UpdateDeviceList()
         {
             this.devs = AnDevice_GetOpaqueList(this.ctx, out outndevs, out err);
             return outndevs;
         }
 
+        /// <summary>
+        /// Send a color set command to a physical Glow device
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="dev"></param>
+        /// <param name="r"></param>
+        /// <param name="g"></param>
+        /// <param name="b"></param>
+        /// <returns> (See error.c in libantumbra repo)
+        /// 0 - Success
+        /// 1 - Disconnected
+        /// 2 - Memory allocation failed
+        /// 3 - libusb error
+        /// 4 - Device in wrong state for operation
+        /// 5 - Value out of range
+        /// 6 - Command unsupported
+        /// 7 - Command failure
+        /// 8 - Unspecified protocol error
+        /// </returns>
         public int SetDeviceColor(int index, IntPtr dev, UInt16 r, UInt16 g, UInt16 b)
         {
             LightInfo info;
@@ -50,16 +90,26 @@ namespace Antumbra.Glow.Connector
             return AnLight_Set_S(this.ctx, dev, out info, r, g, b);
         }
 
+        /// <summary>
+        /// Close a Glow device connection
+        /// </summary>
+        /// <param name="dev">Device pointer</param>
         public void CloseDevice(IntPtr dev)
         {
             AnDevice_Close(this.ctx, dev);
         }
 
+        /// <summary>
+        /// Free the list of devices
+        /// </summary>
         public void FreeList()
         {
             AnDevice_FreeOpaqueList(this.devs);
         }
 
+        /// <summary>
+        /// Free context
+        /// </summary>
         public void FreeCtx()
         {
             AnCtx_Deinit(this.ctx);
