621ecf217005eb73445b1e9f1e4f6ec8d01c6f76
walshie4 <adam@antumbra.io>
2015-08-09 17:47:26 -0400

    Adds ConnectionEventObserver and ConnectionEventSource interfaces for observing when the connected device count changes

---
+++ Antumbra/Observer/Connection/ConnectionEventObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antumbra.Glow.Observer.Connection
+{
+    public interface ConnectionEventObserver
+    {
+        void ConnectionUpdate(int deviceCount);
+    }
+}
